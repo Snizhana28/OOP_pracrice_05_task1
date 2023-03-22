b310a977ee17b660ef7f1a9bf32c9db9eb6e658a
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-03-22 11:46:26 +0200

    screens

--- screens.h
+++ screens.h
@@ -1,1 +1,14 @@
-#pragma once
+#include "BookProvider.h"
+
+class Screns
+{
+public:
+	static void Home()
+	{
+		Book book("Richard Branson", "My Ryles", "Alpina", 2015, 10, 360);
+		Book book_copy(book);
+		book_copy.set_year(2022);
+		BookProvider::Print(book.copy());
+		BookProvider::Print(book_copy.copy());
+	}
+};

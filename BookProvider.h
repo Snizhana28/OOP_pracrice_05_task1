9f4298fc0f10419c7e11b92bf4798dfd8c24eaa3
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-03-22 11:44:29 +0200

    Provider

--- BookProvider.h
+++ BookProvider.h
@@ -1,1 +1,28 @@
-#pragma once
+#include "Book.h"
+
+class BookProvider
+{
+public:
+	
+	static void Print(Book* obj)
+	{
+		cout << "Avtor: " << obj->avtor << endl;
+		cout << "Name: " << obj->name << endl;
+		cout << "Publishing house: " << obj->publishing_house << endl;
+		cout << "Year: " << obj->year << endl;
+		cout << "Number of books: " << obj->number_of_books << endl;
+		cout << "Number of pages: " << obj->number_of_pages << endl << endl;
+
+	}
+
+	static void Print(Book obj)
+	{
+		cout << "Avtor: " << obj.avtor << endl;
+		cout << "Name: " << obj.name << endl;
+		cout << "Publishing house: " << obj.publishing_house << endl;
+		cout << "Year: " << obj.year << endl;
+		cout << "Number of books: " << obj.number_of_books << endl;
+		cout << "Number of pages: " << obj.number_of_pages << endl << endl;
+
+	}
+};

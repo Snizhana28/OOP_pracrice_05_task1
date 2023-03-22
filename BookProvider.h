5469abd150feba06349b153e85e852cb9b93d44e
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-03-22 11:51:21 +0200

    Provider

--- BookProvider.h
+++ BookProvider.h
@@ -6,23 +6,23 @@ public:
 	
 	static void Print(Book* obj)
 	{
-		cout << "Avtor: " << obj->avtor << endl;
-		cout << "Name: " << obj->name << endl;
-		cout << "Publishing house: " << obj->publishing_house << endl;
-		cout << "Year: " << obj->year << endl;
-		cout << "Number of books: " << obj->number_of_books << endl;
-		cout << "Number of pages: " << obj->number_of_pages << endl << endl;
+		cout << "Avtor: " << obj->get_avtor() << endl;
+		cout << "Name: " << obj->get_name() << endl;
+		cout << "Publishing house: " << obj->get_publishing_house() << endl;
+		cout << "Year: " << obj->get_year() << endl;
+		cout << "Number of books: " << obj->get_number_of_books() << endl;
+		cout << "Number of pages: " << obj->get_number_of_pages() << endl << endl;
 
 	}
 
 	static void Print(Book obj)
 	{
-		cout << "Avtor: " << obj.avtor << endl;
-		cout << "Name: " << obj.name << endl;
-		cout << "Publishing house: " << obj.publishing_house << endl;
-		cout << "Year: " << obj.year << endl;
-		cout << "Number of books: " << obj.number_of_books << endl;
-		cout << "Number of pages: " << obj.number_of_pages << endl << endl;
+		cout << "Avtor: " << obj.get_avtor() << endl;
+		cout << "Name: " << obj.get_name() << endl;
+		cout << "Publishing house: " << obj.get_publishing_house() << endl;
+		cout << "Year: " << obj.get_year() << endl;
+		cout << "Number of books: " << obj.get_number_of_books() << endl;
+		cout << "Number of pages: " << obj.get_number_of_pages() << endl << endl;
 
 	}
 };
